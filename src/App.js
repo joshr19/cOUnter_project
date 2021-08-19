e747186ea302ec988166a021fc065b20c5904ff9
joshr19 <joshuaradeen.19ee048@mbcet.ac.in>
2021-08-19 21:44:55 +0530

    initial

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import {useState} from 'react'
+import './App.css';
+
+function App() {
+  let [num,setNum]=useState(0)
+  const increment=()=>{
+    setNum(num+1)
+
+  }
+  const decrement=()=>{
+    setNum(num-1)
+    
+  }
+  return (
+    <div>
+    <div id="main_container">
+     <h1>{num}</h1>
+     <div id="btn_collection">
+     <button onClick={increment}>ADD</button>
+     <button onClick={decrement}>SUB</button>
+     </div>
+     </div>
+     </div>
+  );
+}
+
+export default App;
